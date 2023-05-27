65bf4402e90d4deb05fa10b0046657f2ed9b73de
nobody-27 <nobodypal27@gmail.com>
2023-05-28 01:42:37 +0530

    cache files

--- gs1/Token_Authentication/models.py
+++ gs1/Token_Authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Token_Testing(models.Model):
+    name = models.CharField(max_length=100)
+    roll = models.CharField(max_length=243)
+    description = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return super(self.name).__str__()
+    
